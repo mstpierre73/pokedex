5ac924b656eb1a13b65b28e6bbcc5baa87a6adae
mstpierre73 <naranjaqc@hotmail.com>
2019-07-29 11:47:20 -0400

    Updates

---
+++ precache-manifest.5ecff153ce09e9572c0b790d75316672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66323ca293473e4b1a352b4de30df58d",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "e09453db49bb07a6df36",
+    "url": "/pokedex/static/css/main.37fef722.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/pokedex/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "e09453db49bb07a6df36",
+    "url": "/pokedex/static/js/main.da7c3c94.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
